c56e7920e02a5960fc3019fb66dda37eb732f543
zubat187 <krotilia3@gmail.com>
2020-09-21 07:01:45 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >>N;
+for (int i=0; i<=N; i++)
+{
+  cout <<"Proverka na chetnost" <<endl;
+  if (i%2==1)
+  cout <<i <<endl;
+  
+}
+return 0;
+}
